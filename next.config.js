6f91ab60792782c2be5fd849338c393e4e186907
shifuguru <54631241+shifuguru@users.noreply.github.com>
2026-04-23 15:53:11 +1200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/web-builder',
   images: {
     unoptimized: true
   },
-  basePath: '/web-builder',
 }
 
 module.exports = nextConfig
